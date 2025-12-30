5b934ff698de5e48e6e0b52c8711c85300bcef14
fronthack9-glitch <fronthack9@gmail.com>
2025-12-30 14:00:45 +0400

    Update ConfigService.js

--- service/ConfigService.js
+++ service/ConfigService.js
@@ -2,17 +2,22 @@ import { config } from 'dotenv'
 
 export class ConfigService {
   constructor() {
-    const {error, parsed} = config();
-    if (error || !parsed) {
-      throw new Error("Ошибка прочтения env");
+    if (process.env.NODE_ENV !== 'production') {
+      const { error, parsed } = config()
+      if (error || !parsed) {
+        throw new Error('Ошибка прочтения env')
+      }
+      this.data = parsed
     }
-    this.data = parsed;
+    else {
+      this.data = process.env;
+    }
   }
   get(key) {
-    const res = this.data[key];
+    const res = this.data[key]
     if (!res) {
-      throw new Error("Такого ключа нет в env");
+      throw new Error('Такого ключа нет в env')
     }
-    return res;
+    return res
   }
 }
