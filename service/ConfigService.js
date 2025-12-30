c0d47c9d8bf9faef9354247def5e80d9cc686380
fronthack9-glitch <fronthack9@gmail.com>
2025-12-30 14:06:15 +0400

    Refactor ConfigService for improved env variable handling
    Refactor ConfigService to simplify environment variable access and error handling.

--- service/ConfigService.js
+++ service/ConfigService.js
@@ -1,23 +1,13 @@
 import { config } from 'dotenv'
 
 export class ConfigService {
-  constructor() {
-    if (process.env.NODE_ENV !== 'production') {
-      const { error, parsed } = config()
-      if (error || !parsed) {
-        throw new Error('Ошибка прочтения env')
+    get(key) {
+      const value = process.env[key]
+  
+      if (!value) {
+        throw new Error(`ENV "${key}" не задан`)
       }
-      this.data = parsed
+  
+      return value
     }
-    else {
-      this.data = process.env;
-    }
-  }
-  get(key) {
-    const res = this.data[key]
-    if (!res) {
-      throw new Error('Такого ключа нет в env')
-    }
-    return res
-  }
 }
